9a434baaf77f0fd7919b7ad677e6b7628caa9f2c
DinaMazur <146865201+DinaMazur@users.noreply.github.com>
2025-05-31 13:59:42 +0300

    finish

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const timerDisplay = document.getElementById("timer");
+const timerDisplay = document.getElementById("timmer");
 let minutes = 60;
 
-const timer = setInterval(() => {
+const timmer = setInterval(() => {
     minutes--;
     timerDisplay.textContent = `${minutes}:00`;
 
@@ -10,7 +10,36 @@ const timer = setInterval(() => {
     }
 
     if (minutes === 0) {
-        clearInterval(timer);
+        clearInterval(timmer);
         alert("Час вийшов!");
     }
 }, 60000);
+
+
+
+// завдання 2
+
+
+let time = 30.000;
+let interval;
+const timerElement = document.getElementById("timer");
+const startButton = document.getElementById("startButton");
+
+function startTimer() {
+    startButton.disabled = true;
+    interval = setInterval(() => {
+        time -= 0.001;
+        timerElement.textContent = time.toFixed(3);
+
+        if (time <= 10.000) {
+            timerElement.classList.add("animation");
+        }
+
+        if (time <= 0) {
+            clearInterval(interval);
+            time = 0.000;
+            timerElement.textContent = time.toFixed(3);
+            startButton.disabled = false;
+        }
+    }, 1);
+}
